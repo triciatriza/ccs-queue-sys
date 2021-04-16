7d76a5b23634623d7bda5cb42919ba2aefa13c48
triciatriza <destrizapatricia@gmail.com>
2021-04-17 01:16:52 +0800

    added animation on CSQueuingSyster
    added toggle menu animation, connected toggle menu buttons to their respective pages, deleted ui_animations and main_menu python files

--- CompStud Queuing System/main.py
+++ CompStud Queuing System/main.py
@@ -1,8 +1,12 @@
 import sys
-from PyQt5 import QtWidgets
+import platform
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtCore import QPropertyAnimation
 from PyQt5.QtWidgets import QDialog, QApplication, QMainWindow
+from PyQt5.QtGui import QIcon, QPixmap
 from PyQt5.uic import loadUi
-import resource_rc
+from CSQueuingSystem import *
+
 
 def loadMenu(menu):
     widget.addWidget(menu)
@@ -12,21 +16,45 @@ class Login(QDialog):
     def __init__(self):
         super(Login, self).__init__()
         loadUi(r"ui\login_page.ui", self)
-        self.gotoregister_btn.clicked.connect(lambda:loadMenu(Register()))
+        self.gotoregister_btn.clicked.connect(lambda: loadMenu(Register()))
 
 class Register(QDialog):
     def __init__(self):
         super(Register, self).__init__()
         loadUi(r"ui\register_page.ui", self)
-        self.gotologin_btn.clicked.connect(lambda:loadMenu(Login()))
+        self.gotologin_btn.clicked.connect(lambda: loadMenu(Login()))
 
 class CSQueue(QMainWindow):
     def __init__(self):
-        super(CSQueue, self).__init__()
-        loadUi(r"ui\CSQueuingSystem.ui", self)
+        QMainWindow.__init__(self)
+        self.ui = Ui_ComputerStudiesQueuingSystem()
+        self.ui.setupUi(self)
+        self.ui.Btn_Toggle.clicked.connect(lambda: self.slide_leftmenu())
+        self.ui.queue_button.clicked.connect(lambda: self.ui.stackedWidget.setCurrentWidget(self.ui.queue_page))
+        self.ui.roomreservation_button.clicked.connect(lambda: self.ui.stackedWidget.setCurrentWidget(self.ui.roomreservation_page))
+        self.ui.appointment_button.clicked.connect(lambda: self.ui.stackedWidget.setCurrentWidget(self.ui.appointment_page))
+        self.ui.scholarquests_button.clicked.connect(lambda: self.ui.stackedWidget.setCurrentWidget(self.ui.scholarquests_page))
+        self.ui.account_button.clicked.connect(lambda: self.ui.stackedWidget.setCurrentWidget(self.ui.account_page))
+        self.ui.settings_button.clicked.connect(lambda: self.ui.stackedWidget.setCurrentWidget(self.ui.settings_page))
+        self.show()
+
+    def slide_leftmenu(self):
+        width = self.ui.frame_left_menu.width()
+
+        if width == 70:
+            newwidth = 225
+        else:
+            newwidth = 70
+
+        self.animation = QPropertyAnimation(self.ui.frame_left_menu, b"minimumWidth")
+        self.animation.setDuration(250)
+        self.animation.setStartValue(width)
+        self.animation.setEndValue(newwidth)
+        self.animation.setEasingCurve(QtCore.QEasingCurve.InOutQuart)
+        self.animation.start()
 
 app = QApplication(sys.argv)
-mainwindow = Login()
+mainwindow = CSQueue()
 widget = QtWidgets.QStackedWidget()
 widget.addWidget(mainwindow)
 widget.show()
